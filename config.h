b6b47c2ec49bbd2bd92fd1e92246589ba037762a
Dez113 <dlyakontakt@gmail.com>
2023-08-20 18:12:05 +0300

    one shot functionality

--- config.h
+++ config.h
@@ -4,6 +4,7 @@
 see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
 for more options.
 */
+
 #define ONESHOT_TAP_TOGGLE 2
 #define ONESHOT_TIMEOUT 5000
 #define TIME_CODE_DELAY 1000
